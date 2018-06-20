a6c111202c73a20a6e4299f8b4c3bf7b872b091a
ylambers <y.lambers@outlook.com>
2018-06-20 20:30:58 +0200

    ado

---
+++ ado/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSpeed test = new TestSpeed();
+
+            test.LoopTime(3000);
+
+        }
+    }
+}
---
+++ ado/TestSpeed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ado
+{
+    class TestSpeed
+    {
+        public TestSpeed()
+        {
+
+        }
+
+        public void LoopTime(int time)
+        {
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            for (int index = 0; index < time; index++)
+            {
+                Console.Write("");
+            }
+
+            sw.Stop();
+            
+            Console.WriteLine(sw.ElapsedMilliseconds + " ms");
+            Console.ReadKey();
+        }
+    }
+}
